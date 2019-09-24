b23e1c78c185bf91cd6500ad29ba409a389b1cf0
Evan Miller <evanmiller67@gmail.com>
2019-09-24 15:55:36 -0400

    Give some template usage

--- _tmpl.py
+++ _tmpl.py
@@ -8,4 +8,4 @@ date (updated): 2019-09-16
 
 ## Start of all the things
 ## Bash to create this file: 
-printf "#/usr/bin/env python3 \n\"\"\"\ntitle: filename.py\nauthor: $(whoami)\ndate(created): $(date "+%Y-%m-%d %H:%M:%S %Z")\ndate (updated): $(date "+%Y-%m-%d %H:%M:%S %Z")\n\"\"\"\n\n" >> $1.py
+## $ filename='exercises/12_html_decorator.py' && printf "#/usr/bin/env python3 \n\"\"\"\ntitle: $(echo $filename)\nauthor: $(whoami)\ndate(created): $(date "+%Y-%m-%d %H:%M:%S %Z")\ndate (updated): $(date "+%Y-%m-%d %H:%M:%S %Z")\n\"\"\"\n\n\n" >> $(echo $filename)
